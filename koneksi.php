a9c9d76d5e162aa450ecb5e8ab8dcd1828caf069
stfns2896 <stefanusekaprastya@gmail.com>
2019-07-23 14:09:36 +0700

    Ignoring koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php // Ganti sama punyamu bro,
-$server		= "localhost";
-$username	= "root";
-$password	= "test";
-$name	= "rekapsdm";
-
-$connect	= mysqli_connect($server, $username, $password) or die("Koneksi ke database gagal");
-$select_db	= mysqli_select_db($connect, $name) or die("Tidak ada database yang dipilih");
