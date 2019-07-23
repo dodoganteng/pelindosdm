89208da5622ee141d3cec9af24f8c806e52d3d2c
dodoganteng <ronaldo.grasberg98@gmail.com>
2019-07-23 20:46:50 +0700

    commit after resolve conflict

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php // Ganti sama punyamu bro,
 $server		= "localhost";
 $username	= "root";
@@ -7,13 +6,3 @@ $name	= "rekapsdm";
 
 $connect	= mysqli_connect($server, $username, $password) or die("Koneksi ke database gagal");
 $select_db	= mysqli_select_db($connect, $name) or die("Tidak ada database yang dipilih");
-=======
-<?php // Ganti sama punyamu bro,
-$server		= "localhost";
-$username	= "root";
-$password	= "";
-$name	= "rekapsdm";
-
-$connect	= mysqli_connect($server, $username, $password) or die("Koneksi ke database gagal");
-$select_db	= mysqli_select_db($connect, $name) or die("Tidak ada database yang dipilih");
->>>>>>> 40bb249c1a9a0d9c5a5b14c29cf9c510e03ae1db
