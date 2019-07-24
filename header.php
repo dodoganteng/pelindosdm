7b990430fe7352b0085fecd29eb4c8deb8ef2da5
dodoganteng <ronaldo.grasberg98@gmail.com>
2019-07-24 13:17:59 +0700

    delete button home

--- header.php
+++ header.php
@@ -18,18 +18,6 @@
     <a class="navbar-brand" href="#">
       <h2>Diklatku</h2>
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-
-      </ul>
-    </div>
   </nav>
   <br></br>
 <div class="col-12">
